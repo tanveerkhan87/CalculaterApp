60b65e092307ae45cf966698360696207608cfb3
tanveerkhan87 <Engr.tanveerkhan7@gmail.com>
2023-05-09 00:02:35 +0500

    final comment
    final comments

--- lib/tanveer.dart
+++ lib/tanveer.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
+        // I am tanveer khan
       ),
       home: const HomeScreen(),
     );
